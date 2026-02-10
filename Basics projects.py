18f33a9326853eb04899458a008561159a882151
Sushmita Naik <sushmitanaik075@gmail.com>
2026-02-10 21:43:55 +0530

    Added all project files

--- Basics projects.py
+++ Basics projects.py
@@ -66,20 +66,42 @@
 # best_day=np.argmax(step_walk)
 # print("The best day",best_day)
 
+# import numpy as np
+# sale_data=np.array([
+#     [500,600,700,800,350,850,444],
+#     [333,5605,8900,145,745,2033,456],
+#     [7707,231,2580,147,369,8502,741],
+#     [7809,456,123,357,159,2480,156]
+# ])
+# total_sales_per_week=np.sum(sale_data,axis=1)
+# print("The total sales according to each week is:",total_sales_per_week)
+# total_sales_per_day=np.sum(sale_data,axis=0)
+# print("The total sales per day across all weeks are:",total_sales_per_day)
+# overall_total=np.sum(sale_data)
+# print("The overall total monthly sales",overall_total)
+# highest_total=np.argmax(total_sales_per_week)
+# print("The week which is having highest total sales is:",highest_total)
+# sale_data[sale_data>2000]=2000
+# print("The sales after replacing 2000",sale_data)
+
+
 import numpy as np
-sale_data=np.array([
-    [500,600,700,800,350,850,444],
-    [333,5605,8900,145,745,2033,456],
-    [7707,231,2580,147,369,8502,741],
-    [7809,456,123,357,159,2480,156]
+attendence_data=np.array([
+    [1,0,1,1,1,0,1],
+    [0,0,1,0,1,1,1],
+    [1,1,1,1,1,0,0],
+    [0,0,0,0,1,1,1],
+    [1,1,1,0,0,0,1]
 ])
-total_sales_per_week=np.sum(sale_data,axis=1)
-print("The total sales according to each week is:",total_sales_per_week)
-total_sales_per_day=np.sum(sale_data,axis=0)
-print("The total sales per day across all weeks are:",total_sales_per_day)
-overall_total=np.sum(sale_data)
-print("The overall total monthly sales",overall_total)
-highest_total=np.argmax(total_sales_per_week)
-print("The week which is having highest total sales is:",highest_total)
-sale_data[sale_data>2000]=2000
-print("The sales after replacing 2000",sale_data)
+total_days_present_for_each_student=np.sum(attendence_data,axis=1)
+print("The total days present for each student is",total_days_present_for_each_student)
+student_present_each_day=np.sum(attendence_data,axis=0)
+print("The students were present in each day is:",student_present_each_day)
+totals=np.sum(attendence_data,axis=1)
+highest_attendence=np.argmax(totals)
+print("The student which is having highest attendence is:",highest_attendence)
+less_than_3_days=totals<3
+count=np.sum(less_than_3_days)
+print("the student are having attendance less than 3 days are:",count)
+totals[totals<2]=2
+print("the student attendance below 2 days are replacing",totals)
--- student attendance.py
+++ student attendance.py
@@ -22,44 +22,34 @@
 
 
 import numpy as np
-hospital_data=np.array([
+college_marks=np.array([
     [
-        [98.6, 99.1, 100.2, 97.9],
-        [99.0, 98.7, 101.0, 98.5],
-        [100.1, 99.5, 98.8, 97.6],
-        [98.4, 100.3, 99.2, 98.9],
-        [97.8, 98.6, 99.9, 100.5],
-        [99.4, 98.2, 97.5, 100.0],
-        [98.9, 99.7, 98.3, 97.4]
+        [70,88,48,68],
+        [91,92,83,84],
+        [74,85,96,67],
+        [48,79,53,85],
+        [65,37,29,61]
     ],
     [
-        [99.5, 100.1, 98.7, 97.8],
-        [98.9, 99.2, 100.4, 98.6],
-        [97.6, 98.5, 99.3, 100.0],
-        [99.8, 98.1, 97.9, 100.2],
-        [98.4, 99.6, 100.3, 97.7],
-        [97.5, 98.8, 99.0, 100.6],
-        [99.1, 98.3, 97.4, 100.1]
+        [81,84,86,87],
+        [94,95,96,57],
+        [79,79,86,94],
+        [51,61,72,93],
+        [72,85,46,74]
     ],
     [
-        [100.2, 99.4, 98.6, 97.9],
-        [98.7, 100.5, 99.1, 97.8],
-        [99.3, 98.2, 100.0, 97.6],
-        [97.5, 99.9, 98.4, 100.3],
-        [98.8, 97.7, 99.6, 100.4],
-        [100.1, 98.3, 97.9, 99.2],
-        [97.6, 100.0, 98.5, 99.7]
+        [61,82,93,65],
+        [67,78,98,99],
+        [80,79,65,52],
+        [100,51,83,34],
+        [71,68,75,64]
     ]
+
 ])
-ward_avg=np.mean(hospital_data,axis=(1,2))
-print("Average temparature of ward  is:",ward_avg)
-daily_avg=np.mean(hospital_data,axis=(0,2))
-print("The daily avarege temparature is:",daily_avg)        #axis 0 is ward and, axis 1 is day, and axis 2 is patient
-overall_average=np.mean(hospital_data)
-print("The overall average of the hospital is:",overall_average)
-highest_avg=np.argmax(ward_avg)
-print("Ward with highest average is:",highest_avg+1)
-fever_cases=np.sum(hospital_data>101)
-print("The number of fever cases:",fever_cases)
-hospital_data[hospital_data>104]=104
-print("Replacing safety cap:",hospital_data)
+total_marks_per_class=np.sum(college_marks,axis=0)
+print("The total marks per each class",total_marks_per_class)
+average_per_subjetc=np.mean(college_marks,axis=2)
+print("The average marks per subject is:",average_per_subjetc)
+student_totals=np.sum(college_marks,axis=2)
+best_student=np.argmax(student_totals,axis=1)
+print("Best student index in each class:",best_student)
